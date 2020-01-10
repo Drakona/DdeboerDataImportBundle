8e05257cce7675346bbc6c16d6951ecda6fc0fab
Jarjat Rémi <1244807+Dreeckan@users.noreply.github.com>
2020-01-10 16:16:16 +0100

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,10 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('ddeboer_data_import');
+        $treeBuilder = new TreeBuilder('ddeboer_data_import');
+        $rootNode = method_exists(TreeBuilder::class, 'getRootNode')
+            ? $treeBuilder->getRootNode()
+            : $treeBuilder->root('ddeboer_data_import');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
